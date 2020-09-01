c575673e39787461a72a6527c1553b6df04bb382
Zeyad Zaher <62356734+ZeyadZaher03@users.noreply.github.com>
2020-09-01 21:01:48 +0200

    fix issues

--- js/register.js
+++ js/register.js
@@ -30,7 +30,7 @@ facebookButton.addEventListener("click", (e) => {
             //   This gives you a Google Access Token. You can use it to access the Google API.
             // var token = result.credential.accessToken;
             // The signed-in user info.
-            db.ref(`users/${email}`).push({
+            db.ref(`users/${uid}`).push({
                 name
             }).then((response) => {
                 Cookies.set("uid", uid)
