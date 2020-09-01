d0e0901b8a58e88c9d2c4cbdee3a275518087bbc
Zeyad Zaher <62356734+ZeyadZaher03@users.noreply.github.com>
2020-09-01 20:20:37 +0200

    fix issue

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
-auth.onAuthStateChanged((user) => {
-    if (user || (!!Cookies.get("uid")) == true) {
+auth.onAuthStateChanged(() => {
+    if ((!!Cookies.get("uid")) == true) {
         return window.location.replace("index.html");
     }
 });
