317f1dc47186a998176c3b92ecfe20a64a63fada
Zeyad Zaher <62356734+ZeyadZaher03@users.noreply.github.com>
2020-09-08 14:47:21 +0200

    testing

--- js/article.js
+++ js/article.js
@@ -1,8 +1,9 @@
-authintication()
-menuNavigationSwitch()
-if (!Cookies.get("uid")) {
-    location.replace("index.html")
-}
+authintication();
+menuNavigationSwitch();
+// if (!Cookies.get("uid")) {
+//     location.replace("index.html")
+// }
+
 const runArticle = () => {
     const getDisplay = (item) => getComputedStyle(item).display;
 
@@ -15,7 +16,8 @@ const runArticle = () => {
         inputSelector,
         buttonSelector,
         maxNumber,
-        arrayOfValues
+        arrayOfValues,
+        toLowerCase
     }) => {
         const container = document.querySelector(containerSelector);
         const addItemInput = document.querySelector(inputSelector);
@@ -74,7 +76,14 @@ const runArticle = () => {
             if (getDisplay(addItemInput) == "none") return inputItemVisAnimation();
             if (!addItemInput.value) return;
             if (getDisplay(addItemInput) !== "none") {
-                arrayOfValues.push(addItemInput.value);
+                if (toLowerCase) {
+                    if (arrayOfValues.indexOf(addItemInput.value.toLowerCase()) !== -1) return
+                    arrayOfValues.push(addItemInput.value.toLowerCase());
+                } else {
+                    if (arrayOfValues.indexOf(addItemInput.value) !== -1) return
+                    arrayOfValues.push(addItemInput.value);
+                }
+
                 inputItemHideAnimation();
                 createElements();
                 addItemInput.value = "";
@@ -88,6 +97,7 @@ const runArticle = () => {
         buttonSelector: "#addtagButton",
         maxNumber: 5,
         arrayOfValues: tagsArray,
+        toLowerCase: true
     };
 
     const addWritersOption = {
@@ -110,65 +120,139 @@ const runArticle = () => {
     tags(addWritersOption);
     tags(addEditorsOption);
 
-
     CKEDITOR.replace("editor1");
     const addArticleForm = document.querySelector(".article-form-container");
     const addArticleButton = document.querySelector(".publish-button");
     const articleContent = document.querySelector("#article-content");
     const articletime = document.querySelector("#reading-time");
 
-    const addArticle = () => {
-        const article = CKEDITOR.instances.editor1.getData();
-        articleContent.innerHTML = article
+    const articleImageView = () => {
+        const articleImageInput = document.querySelector("#articleImage");
+        const createImageContainer = (src) => {
+            const imgContainerDiv = document.createElement('div')
+            const imgEle = document.createElement('img')
+            const iconEle = document.createElement('i')
 
-        $('#article-content').readingTime({
-            readingTimeAsNumber: true,
-            readingTimeTarget: $('#reading-time'),
+            imgEle.src = src
+            imgContainerDiv.classList.add("input-file-image")
+
+            iconEle.classList.add("fas", "fa-times", "input-file-close-icon")
+            imgContainerDiv.appendChild(imgEle)
+            imgContainerDiv.appendChild(iconEle)
+            return imgContainerDiv
+        }
+        articleImageInput.addEventListener("change", () => {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                const articleImage = document.querySelector(".input-image")
+                articleImage.classList.add("input-image-with-image")
+                articleImage.innerHTML = ``;
+                articleImage.appendChild(createImageContainer(e.target.result))
+            };
+            reader.readAsDataURL(articleImageInput.files[0]);
+
+        });
+        removeArticleImage()
+    }
+
+    const removeArticleImage = () => {
+
+        document.querySelector('.input-image').addEventListener("click", (e) => {
+            const removeIconBtn = document.querySelector(".input-file-close-icon")
+            if (!!removeIconBtn) {
+                e.preventDefault()
+                removeIconBtn.addEventListener("click", (e) => {
+                    e.preventDefault()
+                    const articleImageInput = document.querySelector("#articleImage");
+                    const articleImage = document.querySelector(".input-image")
+                    articleImage.classList.remove("input-image-with-image")
+                    articleImageInput.files[0] = ""
+                    articleImage.innerHTML = `<svg enable-background="new 0 0 420.8 420.8" version="1.1" viewBox="0 0 420.8 420.8"
+                              xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                              <path
+                                d="m406.8 96.4c-8.4-8.8-20-14-33.2-14h-66.4v-0.8c0-10-4-19.6-10.8-26-6.8-6.8-16-10.8-26-10.8h-120c-10.4 0-19.6 4-26.4 10.8s-10.8 16-10.8 26v0.8h-66c-13.2 0-24.8 5.2-33.2 14-8.4 8.4-14 20.4-14 33.2v199.2c0 13.2 5.2 24.8 14 33.2 8.4 8.4 20.4 14 33.2 14h326.4c13.2 0 24.8-5.2 33.2-14 8.4-8.4 14-20.4 14-33.2v-199.2c0-13.2-5.2-24.8-14-33.2zm-6.8 232.4h-0.4c0 7.2-2.8 13.6-7.6 18.4s-11.2 7.6-18.4 7.6h-326.4c-7.2 0-13.6-2.8-18.4-7.6s-7.6-11.2-7.6-18.4v-199.2c0-7.2 2.8-13.6 7.6-18.4s11.2-7.6 18.4-7.6h77.2c6 0 10.8-4.8 10.8-10.8v-11.6c0-4.4 1.6-8.4 4.4-11.2s6.8-4.4 11.2-4.4h119.6c4.4 0 8.4 1.6 11.2 4.4s4.4 6.8 4.4 11.2v11.6c0 6 4.8 10.8 10.8 10.8h77.2c7.2 0 13.6 2.8 18.4 7.6s7.6 11.2 7.6 18.4v199.2z" />
+                              <path
+                                d="m210.4 130.8c-27.2 0-52 11.2-69.6 28.8-18 18-28.8 42.4-28.8 69.6s11.2 52 28.8 69.6c18 18 42.4 28.8 69.6 28.8s52-11.2 69.6-28.8c18-18 28.8-42.4 28.8-69.6s-11.2-52-28.8-69.6-42.4-28.8-69.6-28.8zm54.4 153.2c-14 13.6-33.2 22.4-54.4 22.4s-40.4-8.8-54.4-22.4c-14-14-22.4-33.2-22.4-54.4s8.8-40.4 22.4-54.4c14-14 33.2-22.4 54.4-22.4s40.4 8.8 54.4 22.4c14 14 22.4 33.2 22.4 54.4 0.4 21.2-8.4 40.4-22.4 54.4z" />
+                              <circle cx="352.8" cy="150" r="19.6" />
+                            </svg>`
+                })
+            }
         })
+    }
+
+    articleImageView()
+    const addArticle = async () => {
+        const article = CKEDITOR.instances.editor1.getData();
+        articleContent.innerHTML = article;
+
+        $("#article-content").readingTime({
+            readingTimeAsNumber: true,
+            readingTimeTarget: $("#reading-time"),
+        });
 
         const name = addArticleForm["name"];
         const tagline = addArticleForm["tagline"];
         const description = addArticleForm["description"];
+        const articelImage = addArticleForm["publication_avatar"];
 
         const twiiter = addArticleForm["twitter"];
         const facebook = addArticleForm["facebook"];
         const instagram = addArticleForm["instagram"];
         const email = addArticleForm["email"];
 
-        const est = articletime.innerHTML
+        const image = articelImage.files[0]
+
+        const est = articletime.innerHTML;
         const errorMessages = () => {
-            let emptyValues = 0
+            let emptyValues = 0;
             const errorInput = (ele, errorMessage) => {
                 if (ele.value === "") {
-                    const inputInfo = ele.parentNode.querySelector('.input-text')
-                    const tip = inputInfo.innerHTML
-                    const err = errorMessage
-                    const errClass = "input-text-err"
-                    emptyValues++
-                    inputInfo.classList.add(errClass)
-                    inputInfo.innerHTML = err
+                    const inputInfo = ele.parentNode.querySelector(".input-text");
+                    const tip = inputInfo.innerHTML;
+                    const err = errorMessage;
+                    const errClass = "input-text-err";
+                    emptyValues++;
+                    inputInfo.classList.add(errClass);
+                    inputInfo.innerHTML = err;
                     ele.addEventListener("change", () => {
-                        inputInfo.innerHTML = tip
-                        inputInfo.classList.remove(errClass)
-                    })
+                        inputInfo.innerHTML = tip;
+                        inputInfo.classList.remove(errClass);
+                    });
                 }
+            };
+            if (!image) {
+                emptyValues++;
             }
             if (article === "") {
-                emptyValues++
-                const errArea = document.querySelector("#article-err-message")
-                const errorMessage = "Please Write the Article body"
+                emptyValues++;
+                const errArea = document.querySelector("#article-err-message");
+                const errorMessage = "Please Write the Article body";
                 const articleBodyInput = document.querySelector("#editor1");
-                errArea.innerHTML = errorMessage
+                errArea.innerHTML = errorMessage;
             }
 
+            errorInput(name, "Name is Required");
+            errorInput(tagline, "tagline is Required");
+            errorInput(description, "description is Required");
 
-            errorInput(name, "Name is Required")
-            errorInput(tagline, "tagline is Required")
-            errorInput(description, "description is Required")
+            return emptyValues;
+        };
+        let imgUrl
+        const getImageUrl = async () => {
+            const storageRef = firebase.storage().ref(`articles`)
+            const fileName = `${new Date()}_${image.name}`
+            const metaData = {
+                contentType: image.type,
+            }
+            const driverImageUrl = await storageRef
+                .child(fileName)
+                .put(image, metaData)
+                .then(async (snapshot) => await snapshot.ref.getDownloadURL())
 
-            return emptyValues
+            return imgUrl = driverImageUrl
         }
-
+        if (image) await getImageUrl()
+        // const uid = Cookies.get("uid");
 
         const articleObj = {
             name: name.value,
@@ -179,39 +263,65 @@ const runArticle = () => {
             instagram: instagram.value,
             email: email.value,
             est,
+            image: imgUrl,
             tags: tagsArray,
             writer: writerArray,
             editor: editorArray,
             article,
-            uid: Cookies.get("uid")
+            uid: 'C16NeLUBm5XfzKSuySJf7Ti1Uw92',
         };
 
         return {
             article: articleObj,
-            numberOfErrors: errorMessages()
+            numberOfErrors: errorMessages(),
         };
     };
 
     const addArticleToDb = async (articleObj) => {
-        await db.ref(`articles`).push(articleObj).then((snapshot) => {
-            id = snapshot.key
-            db.ref(`users/${Cookies.get("uid")}/articles`).push(id)
-        })
-    }
-    addArticleForm.addEventListener("submit", (e) => e.preventDefault());
-    addArticleButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        const article = addArticle().article
-        const errorsInArticle = addArticle().numberOfErrors
-        if (errorsInArticle > 0) return
-        addArticleToDb(article)
-        addArticleForm.reset()
+        await db
+            .ref(`articles`)
+            .push(articleObj)
+            .then((snapshot) => {
+                id = snapshot.key;
+                // db.ref(`users/${Cookies.get("uid")}/articles`).push(id);
+                db.ref(`users/C16NeLUBm5XfzKSuySJf7Ti1Uw92/articles`).push(id);
+            });
+    };
 
+
+    addArticleForm.addEventListener("submit", (e) => e.preventDefault());
+
+    const getDbTags = async () => {
+        const tagRef = await db.ref(`tags`).once("value")
+        const tags = []
+        await tagRef.forEach((tag) => {
+            tags.push(tag.val())
+        })
+        return tags
+    }
+
+    const saveNewTags = async (tags) => {
+        const dbTags = await getDbTags()
+        const newTags = tags.filter(tag => !(dbTags.includes(tag)));
+        newTags.forEach((tag) => db.ref(`tags`).push(tag))
+    }
+
+    addArticleButton.addEventListener("click", async (e) => {
+        e.preventDefault();
+        const articleReturn = await addArticle()
+        const article = articleReturn.article;
+        const errorsInArticle = articleReturn.numberOfErrors;
+        if (errorsInArticle > 0) return;
+        if (!!(article.tags.length)) saveNewTags()
+        console.log(article)
+        addArticleToDb(article);
+        addArticleForm.reset();
     });
 
-    CKEDITOR.instances.editor1.on('change', () => {
-        const errArea = document.querySelector("#article-err-message")
-        errArea.innerHTML = ""
+
+    CKEDITOR.instances.editor1.on("change", () => {
+        const errArea = document.querySelector("#article-err-message");
+        errArea.innerHTML = "";
     });
 };
 runArticle();
--- js/profile.js
+++ js/profile.js
@@ -1,2 +1,17 @@
 authintication()
 menuNavigationSwitch()
+
+
+const editDataInput = () => {
+
+}
+
+const getProfileData = () => {
+    const uid = Cookies.get("uid")
+    if (!uid) return
+    db.ref(`users/${uid}`).once("value", (snapshot) => {
+        console.log(snapshot.val())
+    })
+}
+
+getProfileData()
